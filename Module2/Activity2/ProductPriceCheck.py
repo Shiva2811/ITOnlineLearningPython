0a0b1c932bf099c9f89a0ceb2623b5cb72c2bbac
Unique Rana <uniquerana@Uniques-Laptop.local>
2024-11-06 14:20:50 +0000

    Module2

---
+++ Module2/Activity2/ProductPriceCheck.py
@@ -0,0 +1,11 @@
+#Defining products prices
+ferrari = 22000
+toyota = 19000
+
+#Checking the price of the product 
+if ferrari<toyota:
+    print("Price of Ferrari is less than Toyota.")
+elif toyota==ferrari:
+    print("Price of Ferari and Toyota are same.")
+else:
+    print("Price of Toyota is less than Ferrari.")
---
+++ Module2/IndentationExample.py
@@ -0,0 +1,7 @@
+x = 10
+y = 20
+
+if x<y:
+    print("Y is greater than X.")
+else:
+    print("X is greater than Y.")
